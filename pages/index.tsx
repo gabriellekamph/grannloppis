525f5142af0d11339267a85013d90b9b4855cc6e
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2022-03-14 21:52:36 +0100

    feat: add dropdown with basic functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from 'next'
 import Footer from '../components/Footer'
+import DropDown from '../components/DropDown'
 import React from 'react'
 import GrannloppisLogoWhite from '../public/logos/grannloppis_logo_white.svg'
 
 const Home: NextPage = () => {
   return (
     <>
-    <div className="container mx-auto mt-32">
+    <div className="container mx-auto mt-32 flex flex-col justify-items-center">
       <GrannloppisLogoWhite className="max-w-lg p-10 mx-auto" />
-      <p className="text-lg text-center">Välj loppis att delta på:</p>
+      <DropDown />
     </div>
     <Footer />
     </>
