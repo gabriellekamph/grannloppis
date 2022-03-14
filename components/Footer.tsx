9c97aae1dca64e0651e15b78249cbbf6bc2aa873
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2022-03-14 21:20:56 +0100

    wip: basic layout at startpage

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="mx-auto w-screen absolute bottom-5">
+            <p className="text-center">&copy; Copyright 2022 | Grannloppis.se</p>
+        </div>
+    )
+}
+
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next'
+import Footer from '../components/Footer'
+import React from 'react'
+import GrannloppisLogoWhite from '../public/logos/grannloppis_logo_white.svg'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl">Grannloppis</h1>
+    <>
+    <div className="container mx-auto mt-32">
+      <GrannloppisLogoWhite className="max-w-lg p-10 mx-auto" />
+      <p className="text-lg text-center">Välj loppis att delta på:</p>
+    </div>
+    <Footer />
+    </>
   )
 }
 
