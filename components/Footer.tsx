46cedd7f703b1ea135e57900b9598549bfd13396
Gabrielle Kamph <72319394+gabriellekamph@users.noreply.github.com>
2022-03-17 11:02:51 +0100

    feat: connect mapbox to display map with some dummy data
    * wip: connect mapbox and display map of krusboda
    
    * fix: some renaming
    
    * wip: add markers to map and display dymmu data from json file
    
    * feat: add close button to popup in map and some other minor fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="mx-auto w-screen absolute bottom-5">
+        <div className="container mx-auto w-screen absolute bottom-5">
             <p className="text-center">&copy; Copyright 2022 | Grannloppis.se</p>
         </div>
     )
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,83 @@
-const Map = () => {
-    return (
-        <div className="container mx-auto w-11/12 h-60 flex justify-center content-around mt-5 border-solid border-2 border-white">
-            <div>KARTA HÄR</div>
-        </div>
-    )
+import ReactMapGL, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  GeolocateControl,
+} from "react-map-gl"
+import "mapbox-gl/dist/mapbox-gl.css"
+import * as React from "react"
+import { useState, useMemo } from "react"
+
+import HOUSEHOLDS from "../data.json"
+import Pin from "./Pin"
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiZ2FicmllbGxla2FtcGgiLCJhIjoiY2wwcnhpcTJpMDFoZjNpbzVqNjlmM29kbSJ9.oRJWtVkzXeEk0AmmvOfCig"
+// Make .env file work instead and get API key from there
+
+const INITIAL_VIEW_STATE = {
+  latitude: 59.23146869560826,
+  longitude: 18.247962069565833,
+  width: "90vw",
+  height: "90vh",
+  zoom: 14,
 }
+
+const Map = () => {
+  const [popupInfo, setPopupInfo] = React.useState<string|any>(null)
+
+  const pins = useMemo(
+    () =>
+      HOUSEHOLDS.map((address: any, index: any) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={address.longitude}
+          latitude={address.latitude}
+          anchor="bottom"
+        >
+          <Pin onClick={() => {
+            console.log(address)
+            setPopupInfo(address)
+          }
+            } />
+        </Marker>
+      )),
+    []
+  )
+
+  return (
+    <div className="container mx-auto w-11/12 flex flex-col flex-wrap justify-center content-around mt-5">
+      <ReactMapGL
+        initialViewState={INITIAL_VIEW_STATE}
+        style={{ width: 500, height: 450 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={MAPBOX_TOKEN}
+      >
+        <GeolocateControl position="top-left" />
+        <FullscreenControl position="top-left" />
+        <NavigationControl position="top-left" />
+
+
+        {pins}
+
+        {popupInfo && (
+          <Popup
+            anchor="top"
+            longitude={Number(popupInfo.longitude)}
+            latitude={Number(popupInfo.latitude)}
+            closeOnClick={false}
+            onClose={() => setPopupInfo(null)}
+          >
+            <div>
+              <p className="text-black">{popupInfo.address}</p>
+              <p className="text-black">{popupInfo.categories}</p>
+            </div>
+          </Popup>
+        )}
+      </ReactMapGL>
+    </div>
+  );
+};
 
 export default Map
