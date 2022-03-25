9d386a9889daa9a505369ffdf91b5ecff1d9ef6d
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2022-03-25 11:52:52 +0100

    feat: set Krusboda as initial view on map

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -5,13 +5,13 @@ const GoogleMap = () => {
   const { ref, map, google } = useGoogleMaps(
       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     {
-      center: { lat: 0, lng: 0 },
-      zoom: 3,
+      center: { lat: 59.23146869560826, lng: 18.247962069565833 },
+      zoom: 14,
     },
   );
-  console.log(map); // instance of created Map object (https://developers.google.com/maps/documentation/javascript/reference/map)
-  console.log(google); // google API object (easily get google.maps.LatLng or google.maps.Marker or any other Google Maps class)
-  return <div ref={ref} className="container mx-auto mt-8" style={{ width: 400, height: 300 }} />
+  console.log(map)
+  console.log(google)
+  return <div ref={ref} className="container mx-auto mt-8 w-full" style={{ width: 400, height: 350 }} />
 };
 
 export default GoogleMap
