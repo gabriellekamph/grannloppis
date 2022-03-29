82fcb3167d24aeeb73376be5be4492a5ac367e5f
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2022-03-29 15:06:56 +0200

    feat: login modal with submit confirmation

--- components/AddSeller.tsx
+++ components/AddSeller.tsx
@@ -251,9 +251,8 @@ const AddSeller = () => {
                   </button>
                 </div>
               </div>
-
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          <div className="opacity-50 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
     </>
