8f4ff1fde02f10622c38e648d8560cda94dc6303
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2022-04-01 15:46:22 +0200

    feat: show markers with infowindow

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useRef, useState } from 'react'
-import { Loader } from '@googlemaps/js-api-loader'
+import { useEffect, useRef, useState } from "react"
+import { Loader } from "@googlemaps/js-api-loader"
 import { collection, query, onSnapshot } from "firebase/firestore"
 import { db } from "../firebase"
 
 const Map = () => {
 
-    const [activeMarker, setActiveMarker] = useState(null)
-    const [sellers, setSellers] = useState<any>([])
+  const [sellers, setSellers] = useState<any>([])
 
   const googleMap: any = useRef(null)
 
@@ -14,82 +13,94 @@ const Map = () => {
     {
       featureType: "poi",
       stylers: [
-          {
-            visibility: "off",
-          }
-      ]
-    }
+        {
+          visibility: "off",
+        },
+      ],
+    },
   ]
 
-  const handleActiveMarker = (marker: any) => {
-    if (marker === activeMarker) {
-      return
-    }
-    setActiveMarker(marker)
-  }
-
   useEffect(() => {
-    fetchSellers()
+    fetchSellers();
   }, [])
 
-  // Fetch data from firebase and store in sellers state 
+  // Fetch data from firebase and store in sellers state
 
-  const fetchSellers = async() => {
-
-    const q = await query(collection(db, 'sellers'))
+  const fetchSellers = async () => {
+    const q = await query(collection(db, "sellers"));
 
     const getData = onSnapshot(q, (querySnapshot) => {
-      let allSellers:any = []
+      let allSellers: any = [];
       querySnapshot.forEach((doc) => {
-        const data = doc.data()
-        allSellers.push(data)
-      })
-      setSellers(allSellers)
-    })
-  }
+        const data = doc.data();
+        allSellers.push(data);
+      });
+      setSellers(allSellers);
+    });
+  };
 
   useEffect(() => {
-
-    // Load Google Maps 
+      
+    // Load Google Maps
 
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-      version: 'weekly',
-    })
+      version: "weekly",
+    });
 
-    let map
+    let map: any;
 
     loader.load().then(() => {
-      const google = window.google
-      const defaultCenter = {lat: 59.23146869560826, lng: 18.247962069565833}
+      const google = window.google;
+      const defaultCenter = { lat: 59.23146869560826, lng: 18.247962069565833 };
 
       map = new google.maps.Map(googleMap.current, {
         center: defaultCenter,
         zoom: 15,
-        styles: hidePois
-      })
+        styles: hidePois,
+      });
 
       // Show markers on map from firebase data
 
       for (let i = 0; i < sellers.length; i++) {
-        const seller = sellers[i];
+        const seller = sellers[i]
         const lat = parseFloat(seller.location.lat)
         const lng = parseFloat(seller.location.lng)
 
-        new google.maps.Marker({
-            position: { lat: lat, lng: lng},
-            map,
-        })
-    }
-    })
-  
-    })
+        const marker = new google.maps.Marker({
+          position: { lat: lat, lng: lng },
+          map,
+        });
+
+        // Open popup on marker click
+
+      const sellerInfo = `<div class="content">
+      <p><strong>${seller.address}</strong><br /></p>
+      <p>${seller.categories.join(', ')}</p><br />
+      <p>${seller.info}</p></div>`
+
+      const infowindow = new google.maps.InfoWindow({
+          content: sellerInfo
+      })
+
+      marker.addListener('click', () => {
+          infowindow.open({
+              anchor: marker,
+              map,
+              shouldFocus: false,
+          })
+      })
+      }
+
+      
+    });
+  });
 
   return (
-      <div className="map-container">
-        <div id="map" ref={googleMap} />
-      </div>
-    )
-}
+    <div className="map-container text-black">
+      <div id="map" ref={googleMap} />
+    </div>
+  );
+};
 
-export default Map
+export default Map;
