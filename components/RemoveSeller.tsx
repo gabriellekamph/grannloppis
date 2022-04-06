f31d511786ff2841f52dd9006b4ac4e86a01ab7c
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2022-04-06 16:06:30 +0200

    style: remove uppercase text from buttons

--- components/RemoveSeller.tsx
+++ components/RemoveSeller.tsx
@@ -53,14 +53,14 @@ const RemoveSeller = () => {
               </h1>
               <div className="flex items-center justify-center text-black pt-6">
                 <button
-                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  className="text-red-500 background-transparent font-bold px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                   type="button"
                   onClick={() => setShowModal(false)}
                 >
                   Avbryt
                 </button>
                 <button
-                  className="bg-emerald-500 text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  className="bg-emerald-500 text-black active:bg-emerald-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                   type="button"
                   onClick={removeConfirmed}
                 >
